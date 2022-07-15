963d3ce3d31be845d7e3e1ee18c7cf695ab2b2d3
James Graham <jamesgraham@Jamess-MacBook.fios-router.home>
2022-07-15 12:48:26 -0400

    changed room name

--- smalltalk-adapter.js
+++ smalltalk-adapter.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 function getNewRoom(user) {
-  const roomName = `24/7 Noodln with ${user.fullName}!'`
+  const roomName = `${user.fullName}'s 24/7 Noodln Spot!`
   return fetch(process.env.SMALLTALK_CREATE_TALK_EP, {
     method: 'POST',
     headers: {

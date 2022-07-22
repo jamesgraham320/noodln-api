e42688c4be1027b58e946ef2ddc8b3c01cd18a20
James Graham <jamesgraham@Jamess-MacBook.fios-router.home>
2022-07-22 11:53:21 -0400

    temp HAPPY BDAY IQRAM

---
+++ room-prompts.js
@@ -0,0 +1,11 @@
+const roomPrompts = [
+  "Where are you Noodln from?",
+  "Who is your ideal Noodl partner?",
+  "Talk about your favorite Noodln experience",
+  "Embody your inner Noodl today",
+  ""
+]
+
+module.exports = {
+  roomPrompts
+}
---
+++ vonage-adapter.js
@@ -0,0 +1,10 @@
+const Vonage = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+  apiKey: process.env.VONAGE_KEY,
+  apiSecret: process.env.VONAGE_SECRET,
+});
+
+function sendWelcomeText() {
+  vonage.message.sendSms(from )
+}

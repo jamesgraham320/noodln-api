2459d7b88fb19ad631956401c9a4fd0c8685fd56
James Graham <jamesgraham@Jamess-MacBook.local>
2022-08-07 12:04:29 -0500

    fixed org selection

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 // ./src/index.js
 // importing the dependencies
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const api = require('./api');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const api = require("./api");
 
 const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(cookieParser());
 
-
 // defining endpoints
-app.post('/chatters/', api.createChatter);
-app.post('/organizations/', api.createOrganization);
-app.get('/organizations/:orgId', api.getOrganization);
-app.get('/chatters/:orgId', api.sendChattersByOrgId);
+app.post("/chatters/", api.createChatter);
+app.post("/organizations/", api.createOrganization);
+app.get("/organizations/:orgId", api.getOrganization);
+app.get("/chatters/:orgId", api.sendChattersByOrgId);
 
 // starting the server
 app.listen(process.env.PORT || 3001, () => {
-  console.log("current ENV: ", process.env.NODE_ENV)
+  console.log("current ENV: ", process.env.NODE_ENV);
 });
-
